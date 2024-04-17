3ebd5ef1d376cc7a8c74fb5e65a5af6a6a3327c0
yaw.o.k <obengky72@gmail.com>
2024-04-17 12:00:43 +0200

    Correct typo

--- start_docker_app.sh
+++ start_docker_app.sh
@@ -2,5 +2,5 @@
 
 python manage.py migrate 
 # python manage.py collectstatic 
-python manage.py create_defualt_superuser
+python manage.py create_default_superuser
 gunicorn --bind 0.0.0.0:8000 agrichain.wsgi:application
