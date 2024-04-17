70007f48349fe3543be5c30739889512f5b3cc69
yaw.o.k <obengky72@gmail.com>
2024-04-17 11:31:07 +0200

    Add command to create super user on start up.

--- start_docker_app.sh
+++ start_docker_app.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 python manage.py migrate 
-python manage.py collectstatic 
+# python manage.py collectstatic 
+python manage.py create_defualt_superuser
 gunicorn --bind 0.0.0.0:8000 agrichain.wsgi:application
